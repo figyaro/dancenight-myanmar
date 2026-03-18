dee0b428f7531ab58356fc238d80d1b8549e54c1
今村昌史 <imamuramasafumi@MacBook-Air.local>
2026-03-18 11:03:55 +0700

    fix: remove COEP header to restore broken media links from CDN

--- next.config.ts
+++ next.config.ts
@@ -18,10 +18,6 @@ const nextConfig: NextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin',
           },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
         ],
       },
     ];
